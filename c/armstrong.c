489979e7d639b337b2889b315fe512b52acf9af1
varshaig1999 <62078868+varshaig1999@users.noreply.github.com>
2020-10-16 20:25:16 +0530

    checking armstrong number
    Added a new c code file for checking armstrong number

---
+++ c/armstrong.c
@@ -0,0 +1,27 @@
+#this c code will check if the number is armstrong number or not
+
+
+#include <stdio.h>
+int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+       // remainder contains the last digit
+        remainder = originalNum % 10;
+        
+       result += remainder * remainder * remainder;
+        
+       // removing last digit from the original number
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
